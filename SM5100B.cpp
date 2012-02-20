b95f266488fd6a71c55a2b2d67d2221b03cd6e9e
asonge <a@songe.me>
2012-02-19 19:52:50 -0600

    mid-commit commit...what i have done so far

--- SM5100B.cpp
+++ SM5100B.cpp
@@ -18,7 +18,7 @@ void SM5100B::begin() {
 void SM5100B::loop() {
   while(serial.available()) {
     read_line();
-    if(strncmp(buffer,"+SIND: ",7)==0) {
+    if(strncmp(buffer,(const char*) F("+SIND: "),7)==0) {
       int status = -1;
       if(isDigit(buffer[7]) && isDigit(buffer[8])) {
         char a[3] = {buffer[7],buffer[8],0};
@@ -26,7 +26,7 @@ void SM5100B::loop() {
       } else if(isDigit(buffer[7])) {
         status = buffer[7] - '0';
       }
-      Serial.print("Status ");
+      Serial.print(F("Status "));
       Serial.println(status);
       switch(status) {
         case SM_NETWORK_REGISTERED:
@@ -46,36 +46,40 @@ void SM5100B::loop() {
       Serial.println(buffer);
     }
   }
-  if(autoconnect && _phone_state==4 && _lastAutoconnect + 10000 < millis()) {
+  if(_gprs_state!=1 && autoconnect && _phone_state==SM_SMS_READY && _lastAutoconnect + 10000 < millis()) {
     _lastAutoconnect = millis();
-    if(attached()) attach_gprs();
+    if(attached()) {
+      if(attach_gprs()==0) {
+        _gprs_state=1;
+      }
+    }
   }
 }
 
 /*
  * TODO: error codes
  */
-uint8_t SM5100B::attach_gprs() {
+int8_t SM5100B::attach_gprs() {
   // Set up PDP context
-  Serial.println("Setup PDP Context");
+  Serial.println(F("Setup PDP Context"));
   loop();
   serial.print(F("AT+CGDCONT=1,\"IP\",\""));
   serial.print(_apn);
   serial.println('"');
   read_line();
   if(strcmp(buffer, "OK")!=0) {
-    Serial.print("Didn't set up context: ");
+    Serial.print(F("Didn't set up context: "));
     Serial.println(buffer);
     return -2;
   }
   // Activate PDP context
-  Serial.println("Activate PDP Context");
+  Serial.println(F("Activate PDP Context"));
   loop();
   serial.println(F("AT+CGACT=1,1"));
   if(strcmp(buffer, "OK")==0) {
     return 0;
   } else {
-    Serial.print("Couldn't activate context: ");
+    Serial.print(F("Couldn't activate context: "));
     Serial.println(buffer);
     return -3;
   }
@@ -93,7 +97,7 @@ bool SM5100B::attached() {
     read_line(); // Get rid of OK or others...
     return true;
   } else {
-    Serial.print("Is not attached: ");
+    Serial.print(F("Is not attached: "));
     Serial.println(buffer);
     return false;
   }
@@ -123,4 +127,61 @@ void SM5100B::read_line() {
     if(bytes_read < _SS_MAX_RX_BUFF)
       buffer[bytes_read] = 0;
   }
+}
+
+virtual int connect(IPAddress ip, uint16_t port) {
+  //AT+SDATACONF=1,"TCP","211.123.11.21",1234
+  serial.print(F("1,\"TCP\",\""));
+  serial.print(ip);
+  serial.print('"');
+  serial.println(port);
+  read_line();
+}
+virtual int connect(const char *host, uint16_t port) {
+  //AT+SDATACONF=1,"TCP","211.123.11.21",1234
+  serial.print(F("1,\"TCP\",\""));
+  serial.print(host);
+  serial.print('"');
+  serial.println(port);
+}
+virtual size_t write(uint8_t c) {
+  serial.println("1,1");
+  serial.write(c);
+  serial.write(26);
+  return 1;
+}
+virtual size_t write(const uint8_t *buf, size_t size) {
+  serial.print("1,");
+  serial.println(size);
+  size_t = retsize = serial.write(buf, size);
+  serial.write(26);
+  return size_t;
+}
+virtual int available() {
+  // TODO
+  return 0;
+}
+virtual int read() {
+  // TODO
+  return 0;
+}
+virtual int read(uint8_t *buf, size_t size) {
+  // TODO
+  return 0;
+}
+virtual int peek() {
+  // TOOD
+  return 0;
+}
+virtual void flush() {
+  // TODO
+}
+virtual void stop() {
+  // TODO
+}
+virtual uint8_t connected() {
+  // TODO
+}
+virtual operator bool() {
+  // TODO
 }
--- SM5100B.h
+++ SM5100B.h
@@ -6,7 +6,7 @@
 #define sm5100b_h
 
 #include <SoftwareSerial.h>
-#include "SM5100BClient.h"
+#include <IPAddress.h>
 
 enum {
   SM_SIM_REMOVED,
@@ -25,20 +25,34 @@ enum {
 
 class SM5100B {
   public:
-    SM5100B(const char* apn, uint8_t rxPin=2, uint8_t txPin=3, uint16_t baud = 9600L);
-    
-    void loop();
-    void begin();
-    bool ping_modem();
-    int8_t attach_gprs();
-    //bool setup_pdp();
     SoftwareSerial serial;
     bool autoconnect;
+    
+    SM5100B(const char* apn, uint8_t rxPin=2, uint8_t txPin=3, uint16_t baud = 9600L);
+    
+    int8_t attach_gprs();
+    void begin();
+    void loop();
+    bool ping_modem();
+    
+    virtual int connect(IPAddress ip, uint16_t port) =0;
+    virtual int connect(const char *host, uint16_t port) =0;
+    virtual size_t write(uint8_t) =0;
+    virtual size_t write(const uint8_t *buf, size_t size) =0;
+    virtual int available() = 0;
+    virtual int read() = 0;
+    virtual int read(uint8_t *buf, size_t size) = 0;
+    virtual int peek() = 0;
+    virtual void flush() = 0;
+    virtual void stop() = 0;
+    virtual uint8_t connected() = 0;
+    virtual operator bool() = 0;
     
   private:
     uint16_t _baud;
     uint8_t _phone_state;
     uint8_t _network_state;
+    uint8_t _gprs_state;
     bool attached();
     void read_line();
     char buffer[_SS_MAX_RX_BUFF];
